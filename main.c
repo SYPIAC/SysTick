7198cd71a8141a64f729becb0e9be77d0d2672cd
SYPIAC <Reks888@hotmail.com>
2016-09-29 19:54:58 +0300

    First commit
    Flashes LEDs, sends garbage to COM through PA9 and PA10 on USART1

---
+++ main.c
@@ -0,0 +1,234 @@
+/**
+  ******************************************************************************
+  * @file    SysTick/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    19-September-2011
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************  
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup STM32F4_Discovery_Peripheral_Examples
+  * @{
+  */
+
+/** @addtogroup SysTick_Example
+  * @{
+  */ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+GPIO_InitTypeDef GPIO_InitStructure;
+static __IO uint32_t TimingDelay;
+
+/* Private function prototypes -----------------------------------------------*/
+void Delay(__IO uint32_t nTime);
+static void USART_Config(void);
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief   Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  uint8_t ch;
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       file (startup_stm32f4xx.s) before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, refer to
+        system_stm32f4xx.c file
+     */     
+  USART_Config();
+  /* Initialize Leds mounted on STM32F4-Discovery board */
+  STM_EVAL_LEDInit(LED4);
+  STM_EVAL_LEDInit(LED3);
+  STM_EVAL_LEDInit(LED5);
+  STM_EVAL_LEDInit(LED6);
+
+  /* Turn on LED4 and LED5 */
+  STM_EVAL_LEDOn(LED4);
+  STM_EVAL_LEDOn(LED5);
+
+  /* Setup SysTick Timer for 1 msec interrupts.
+     ------------------------------------------
+    1. The SysTick_Config() function is a CMSIS function which configure:
+       - The SysTick Reload register with value passed as function parameter.
+       - Configure the SysTick IRQ priority to the lowest value (0x0F).
+       - Reset the SysTick Counter register.
+       - Configure the SysTick Counter clock source to be Core Clock Source (HCLK).
+       - Enable the SysTick Interrupt.
+       - Start the SysTick Counter.
+    
+    2. You can change the SysTick Clock source to be HCLK_Div8 by calling the
+       SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8) just after the
+       SysTick_Config() function call. The SysTick_CLKSourceConfig() is defined
+       inside the misc.c file.
+
+    3. You can change the SysTick IRQ priority by calling the
+       NVIC_SetPriority(SysTick_IRQn,...) just after the SysTick_Config() function 
+       call. The NVIC_SetPriority() is defined inside the core_cm4.h file.
+
+    4. To adjust the SysTick time base, use the following formula:
+                            
+         Reload Value = SysTick Counter Clock (Hz) x  Desired Time base (s)
+    
+       - Reload Value is the parameter to be passed for SysTick_Config() function
+       - Reload Value should not exceed 0xFFFFFF
+   */
+  if (SysTick_Config(SystemCoreClock / 1000))
+  { 
+    /* Capture error */ 
+    while (1);
+  }
+
+  while (1)
+  {
+    /* Toggle LED3 and LED6 */
+    STM_EVAL_LEDToggle(LED3);
+    STM_EVAL_LEDToggle(LED6);
+
+    /* Insert 50 ms delay */
+    Delay(50);
+
+    /* Toggle LED4 and LED5 */
+    STM_EVAL_LEDToggle(LED4);
+    STM_EVAL_LEDToggle(LED5);
+
+    /* Insert 100 ms delay */
+    Delay(100);
+    
+    /* Send test data through USART */
+    //ch = USART_ReceiveData(USART1);
+    //if(ch) {
+     USART_SendData(USART1, 255);
+    //}
+  }
+}
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in milliseconds.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{ 
+  TimingDelay = nTime;
+
+  while(TimingDelay != 0);
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (TimingDelay != 0x00)
+  { 
+    TimingDelay--;
+  }
+}
+/**
+  * @brief  Configures the USART Peripheral. Uses pins PA9 and PA10 as USART1.
+    USARTx configured as follows:
+        - BaudRate = 9600 baud  
+        - Word Length = 8 Bits
+        - Two Stop Bit
+        - Odd parity
+        - Hardware flow control disabled (RTS and CTS signals)
+        - Receive and transmit enabled
+  * @param  None
+  * @retval None
+  */
+static void USART_Config(void)
+{
+  USART_InitTypeDef USART_InitStructure;
+  GPIO_InitTypeDef 	GPIO_InitStruct;
+
+  // Enable clock for GPIOA
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+  /**
+  * Tell pins PA9 and PA10 which alternating function you will use
+  * @important Make sure, these lines are before pins configuration!
+  */
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
+  // Initialize pins as alternating function
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+  
+/* USARTx configuration ------------------------------------------------------*/
+  /* USARTx configured as follows:
+        - BaudRate = 9600 baud  
+        - Word Length = 8 Bits
+        - Two Stop Bit
+        - Odd parity
+        - Hardware flow control disabled (RTS and CTS signals)
+        - Receive and transmit enabled
+  */
+  USART_InitStructure.USART_BaudRate = 9600;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_2;
+  USART_InitStructure.USART_Parity = USART_Parity_Odd;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+  
+  USART_Init(USART1, &USART_InitStructure);
+  USART_Cmd(USART1, ENABLE);
+}
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
